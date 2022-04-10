f8d421b68bb656c3b4490cc65144cc2f90df5f6d
Matthieu Mezirard <matthieu.mezirard@tutanota.com>
2022-04-10 10:49:30 +0200

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,10 +6,5 @@ from notion_word_data import app
 from notion_word_data import errors
 
 
-def test_get_word_to_find_success():
+def test_get_word_to_find():
     assert ["Example", "en"] in app.get_words_to_find("WORDS.md", 4)
-
-
-def test_get_word_to_find_failure_InvalidDeclarations():
-    with pytest.raises(errors.InvalidDeclarations):
-        app.get_words_to_find("WORDS.md", 1)
